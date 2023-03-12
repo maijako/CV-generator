e673d8354d71bce5496858c73506d9704dc387cc
ensignjava <109529105+maijako@users.noreply.github.com>
2023-03-12 11:17:54 +0000

    style slider sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Main from "./components/Main";
+import Slider from "./components/Slider";
+import img1 from "./assets/fabio-unsplash.jpg";
 
 function App() {
   return (
     <div>
       <Main />
+      <Slider imageSrc={img1}
+      title={"Build your CV"}
+      subtitle={"Stand out from the crowd"}
+      />
+      <Slider imageSrc={img1}
+      title={"Work smart not hard"}
+      subtitle={"Use the built-in AI to help you"}
+      flipped={true}
+      />
     </div>
   );
 }

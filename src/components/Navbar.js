d50a439b0723e98a2e179d18193e9a208f5569b1
ensignjava <109529105+maijako@users.noreply.github.com>
2023-03-12 16:24:13 +0000

    add menu icons for onclicked state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 import "../styles/Navbar.css";
 import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = ({ navbarLinks }) => {
+
+    const [menuClicked, setMenuClicked] = useState(false);
+
+    const toggleMenClick = () => {
+        setMenuClicked(!menuClicked);
+    }
+
+
     return (
         <nav className="navbar">
             <span className="navbar__logo">CV Builder</span>
+
+            {menuClicked ? (
+            <FiX size={25} className="navbar__menu" onClick={toggleMenuClick} />
+            ) : (
+            <FiMenu size={25} className="navbar__menu" onClick={toggleMenuClick} />
+            )}
             <ul className="navbar__list">
                 {navbarLinks.map((item) => {
                     return (

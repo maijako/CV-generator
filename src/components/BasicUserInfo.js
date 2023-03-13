81b1a85a72360fd8f4985c99b53c7e37b0f88fac
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-13 06:30:32 +0000

    Adds the required text fields with their icons

--- src/components/BasicUserInfo.js
+++ src/components/BasicUserInfo.js
@@ -1,22 +1,81 @@
 import React from "react";
-import { TextField, InputAdornment } from '@mui/material';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import { TextField, InputAdornment, Container } from '@mui/material';
+import { AccountCircle, ContactMail, PhoneAndroid } from '@mui/icons-material';
 
 function BasicUserInfo() {
+    const containerStyles = { bgcolor: "#cfe8fc",
+                                display:"flex", 
+                                flexDirection: "column",
+                                alignItems: "flex-start",
+                                marginTop: 10,
+                                padding: 5 };
+    
+    const textFieldStyles = {margin: "15px 0"};
+
     return(
         <div>
-            <TextField
-                id="input-with-icon-textfield"
-                label="TextField"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                        <AccountCircle />
-                        </InputAdornment>
-                    ),
-                }}
-                variant="standard"
-            />
+            <Container maxWidth="sm" sx={containerStyles}>
+
+                <TextField
+                    id="input-first-name"
+                    label="First Name"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <AccountCircle />
+                            </InputAdornment>
+                        ),
+                    }}
+                    placeholder="Enter First Name"
+                    variant="outlined"
+                    sx={textFieldStyles}
+                />
+
+                <TextField
+                    id="input-second-name"
+                    label="Second Name"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <AccountCircle />
+                            </InputAdornment>
+                        ),
+                    }}
+                    placeholder="Enter Second Name"
+                    variant="outlined"
+                    sx={textFieldStyles}
+                />
+
+                <TextField
+                    id="input-email-address"
+                    label="Email ID"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <ContactMail sx={{fontSize: "large"}}/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    placeholder="Enter Email Address"
+                    variant="outlined"
+                    sx={textFieldStyles}
+                />
+
+                <TextField
+                    id="input-phone-number"
+                    label="Phone Number"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <PhoneAndroid sx={{fontSize: "large"}}/>
+                            </InputAdornment>
+                        ),
+                    }}
+                    placeholder="Enter Phone Number"
+                    variant="outlined"
+                    sx={textFieldStyles}
+                />
+            </Container>
         </div>
     );
 };

b76ed892e72db5d912d5b4b34ee8ce57fd3a8869
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-13 21:24:19 +0000

    Combines the different information sections into a single page

--- src/components/cvcomponents/BasicUserInfo.js
+++ src/components/cvcomponents/BasicUserInfo.js
@@ -1,83 +1,78 @@
 import React from "react";
-import { TextField, InputAdornment, Container } from '@mui/material';
+import { TextField, InputAdornment } from '@mui/material';
 import { AccountCircle, ContactMail, PhoneAndroid } from '@mui/icons-material';
-import EducationSection from "./EducationSection";
 
 function BasicUserInfo() {
-    const containerStyles = { bgcolor: "#cfe8fc",
-                                display:"flex", 
-                                flexDirection: "column",
-                                alignItems: "flex-start",
-                                marginTop: 10,
-                                padding: 5 };
+    const containerStyles = { 
+                            display:"flex", 
+                            flexDirection: "column",
+                            alignItems: "flex-start",
+                            padding: 5 };
     
     const textFieldStyles = {margin: "15px 0"};
 
     return(
-        <div>
-            <Container maxWidth="sm" sx={containerStyles}>
+        <div style={containerStyles}>
 
-                <TextField
-                    id="input-first-name"
-                    label="First Name"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <AccountCircle />
-                            </InputAdornment>
-                        ),
-                    }}
-                    placeholder="Enter First Name"
-                    variant="outlined"
-                    sx={textFieldStyles}
-                />
+            <TextField
+                id="input-first-name"
+                label="First Name"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <AccountCircle />
+                        </InputAdornment>
+                    ),
+                }}
+                placeholder="Enter First Name"
+                variant="outlined"
+                sx={textFieldStyles}
+            />
 
-                <TextField
-                    id="input-second-name"
-                    label="Second Name"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <AccountCircle />
-                            </InputAdornment>
-                        ),
-                    }}
-                    placeholder="Enter Second Name"
-                    variant="outlined"
-                    sx={textFieldStyles}
-                />
+            <TextField
+                id="input-second-name"
+                label="Second Name"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <AccountCircle />
+                        </InputAdornment>
+                    ),
+                }}
+                placeholder="Enter Second Name"
+                variant="outlined"
+                sx={textFieldStyles}
+            />
 
-                <TextField
-                    id="input-email-address"
-                    label="Email ID"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <ContactMail sx={{fontSize: "large"}}/>
-                            </InputAdornment>
-                        ),
-                    }}
-                    placeholder="Enter Email Address"
-                    variant="outlined"
-                    sx={textFieldStyles}
-                />
+            <TextField
+                id="input-email-address"
+                label="Email ID"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <ContactMail sx={{fontSize: "large"}}/>
+                        </InputAdornment>
+                    ),
+                }}
+                placeholder="Enter Email Address"
+                variant="outlined"
+                sx={textFieldStyles}
+            />
 
-                <TextField
-                    id="input-phone-number"
-                    label="Phone Number"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <PhoneAndroid sx={{fontSize: "large"}}/>
-                            </InputAdornment>
-                        ),
-                    }}
-                    placeholder="Enter Phone Number"
-                    variant="outlined"
-                    sx={textFieldStyles}
-                />
-                <EducationSection />
-            </Container>
+            <TextField
+                id="input-phone-number"
+                label="Phone Number"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <PhoneAndroid sx={{fontSize: "large"}}/>
+                        </InputAdornment>
+                    ),
+                }}
+                placeholder="Enter Phone Number"
+                variant="outlined"
+                sx={textFieldStyles}
+            />
             
         </div>
     );

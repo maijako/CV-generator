a68786414a9daf62f473f5b0f30d72c47678d8eb
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-14 14:32:15 +0000

    Adds a placeholder component for adding skills using the autocomplete option

--- src/components/cvcomponents/ProfessionalSummary.js
+++ src/components/cvcomponents/ProfessionalSummary.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
 
 function ProfessionalSummary() {
+    const top100Films = [
+        { title: 'The Shawshank Redemption', year: 1994 },
+        { title: 'The Godfather', year: 1972 },
+        { title: 'The Godfather: Part II', year: 1974 },
+        { title: 'The Dark Knight', year: 2008 },
+        { title: '12 Angry Men', year: 1957 }
+    ];
     return(
         <>
             <h3>Professional Summary</h3>
@@ -13,6 +21,22 @@ function ProfessionalSummary() {
                 minRows={3}
                 maxRows={8}
                 sx={{margin: 2, width: "50%"}}
+            />
+
+            <Autocomplete
+                multiple
+                id="tags-outlined"
+                options={top100Films}
+                getOptionLabel={(option) => option.title}
+                defaultValue={[top100Films[1]]}
+                filterSelectedOptions
+                renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="filterSelectedOptions"
+                    placeholder="Favorites"
+                />
+                )}
             />
         </>
     );

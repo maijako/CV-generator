e24f4103d5cd3c12cd0f84d5afc3e6b59c9a350a
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-15 05:55:27 +0000

    Moves the skills field to the Skills Section component

--- src/components/cvcomponents/CVBuilderInfoPage.js
+++ src/components/cvcomponents/CVBuilderInfoPage.js
@@ -4,12 +4,14 @@ import EducationSection from "./EducationSection";
 import { Container } from '@mui/material';
 import ExperienceSection from "./ExperienceSection";
 import ProfessionalSummary from "./ProfessionalSummary";
+import SkillsSection from "./SkillsSection";
 
 function CVBuilderInfoPage() {
     return(
         <Container fixed sx={{bgcolor: "#cfe8fc", marginTop: 10, padding: 3, borderRadius: 2}}>
             <BasicUserInfo />
             <ProfessionalSummary />
+            <SkillsSection />
             <EducationSection />
             <ExperienceSection />
         </Container>
--- src/components/cvcomponents/ProfessionalSummary.js
+++ src/components/cvcomponents/ProfessionalSummary.js
@@ -1,22 +1,8 @@
 import React from "react";
 import { TextField } from '@mui/material';
-import Autocomplete from '@mui/material/Autocomplete';
 
 function ProfessionalSummary() {
-    const skillsList = [
-        { title: 'HTML'},
-        { title: 'CSS'},
-        { title: 'JavaScript'},
-        { title: 'Bootstrap'},
-        { title: 'jQuery'},
-        { title: 'Git and GitHub'},
-        { title: 'ReactJS'},
-        { title: 'Material UI'},
-        { title: 'Tailwind'},
-        { title: 'TypeScript'},
-        { title: 'Cloud Computing'},
-        { title: 'Amazon Web Services (AWS)'}
-    ];
+
     return(
         <>
             <h3>Professional Summary</h3>
@@ -28,23 +14,6 @@ function ProfessionalSummary() {
                 minRows={3}
                 maxRows={8}
                 sx={{margin: 2, width: "50%"}}
-            />
-
-            <Autocomplete
-                multiple
-                id="tags-outlined"
-                options={skillsList}
-                getOptionLabel={(option) => option.title}
-                
-                filterSelectedOptions
-                renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label="Skills Set"
-                    placeholder="Choose Your Skill Set"
-                />
-                )}
-                sx={{margin: "15px 0", width: "50%"}}
             />
         </>
     );

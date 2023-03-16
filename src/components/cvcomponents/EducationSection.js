60e90ae487d44ee73c0476cc2918b87f81e2adca
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-16 17:20:20 +0000

    Adds the wrapping useState function for all the input fields

--- src/components/cvcomponents/EducationSection.js
+++ src/components/cvcomponents/EducationSection.js
@@ -6,62 +6,85 @@ import { TextField, InputAdornment, InputLabel, MenuItem, FormControl, Select } 
 import "./cv-component-styles.css";
 
 function EducationSection() {
+    const [educationInputFields, setEducationInputFields] = useState([{
+                                                                instituteName: "",
+                                                                courseTitle: "",
+                                                                quali: "",
+                                                                graduationDate: ""
+                                                            }]);
     const [startDateValue, setStartDateValue] = useState(null);
     const [endDateValue, setEndDateValue] = useState(null);
     const [qualification, setQualification] = useState("");
     return(
         
         <>
-        <h3>Education Details</h3>
-        <div className="containerStyles">
-            <TextField
-                id="input-institute-name"
-                label="Institute Name"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <SchoolRoundedIcon />
-                        </InputAdornment>
-                    ),
-                }}
-                placeholder="Enter Institute Name"
-                variant="outlined"
-                sx={{margin: "15px 0"}}
-            />
-            <TextField
-                id="input-title-name"
-                label="Title"
-                InputProps={{
-                    startAdornment: (
-                        <InputAdornment position="start">
-                            <SchoolRoundedIcon />
-                        </InputAdornment>
-                    ),
-                }}
-                placeholder="Enter Title"
-                variant="outlined"
-                sx={{margin: "15px 0"}}
-            />
-            <FormControl sx={{ minWidth: 150 }}>
-                <InputLabel id="selectedQualificationLabel">Qualification</InputLabel>
-                <Select
-                    labelId="selectedQualificationLabel"
-                    id="selectedQualification"
-                    value={qualification}
-                    label="Qualification"
-                    onChange={(event) => setQualification(event.target.value)}
-                >
-                    <MenuItem value={"Degree"}>Under Graduate</MenuItem>
-                    <MenuItem value={"Masters"}>Post Graduate</MenuItem>
-                    <MenuItem value={"Certificate"}>Certificate</MenuItem>
-                </Select>
-            </FormControl>
-            
-        </div>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-                <DatePicker sx={{margin: 2}} label="Enter Start Date" value={startDateValue} onChange={(newValue) => setStartDateValue(newValue)}  format="MMM YY"/>
-                <DatePicker sx={{margin: 2}} label="Enter End Date" value={endDateValue} onChange={(newValue) => setEndDateValue(newValue)}  format="MMM YY"/>
-            </LocalizationProvider>
+            <h3>Education Details</h3>
+            {
+                educationInputFields.map((input, index) => {
+                    return(
+                        <div key={index}>
+                            <div className="containerStyles">
+                                <TextField
+                                    id="input-institute-name"
+                                    label="Institute Name"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <SchoolRoundedIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    placeholder="Enter Institute Name"
+                                    variant="outlined"
+                                    sx={{margin: "15px 0"}}
+                                />
+                                <TextField
+                                    id="input-title-name"
+                                    label="Title"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <InputAdornment position="start">
+                                                <SchoolRoundedIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    placeholder="Enter Title"
+                                    variant="outlined"
+                                    sx={{margin: "15px 0"}}
+                                />
+                                <FormControl sx={{ minWidth: 150 }}>
+                                    <InputLabel id="selectedQualificationLabel">Qualification</InputLabel>
+                                    <Select
+                                        labelId="selectedQualificationLabel"
+                                        id="selectedQualification"
+                                        value={qualification}
+                                        label="Qualification"
+                                        onChange={(event) => setQualification(event.target.value)}
+                                    >
+                                        <MenuItem value={"Degree"}>Under Graduate</MenuItem>
+                                        <MenuItem value={"Masters"}>Post Graduate</MenuItem>
+                                        <MenuItem value={"Certificate"}>Certificate</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                
+                            </div>
+                            <LocalizationProvider dateAdapter={AdapterMoment}>
+                                <DatePicker 
+                                    sx={{margin: 2}} 
+                                    label="Enter Start Date"  
+                                    format="MMM YY"
+                                />
+                                <DatePicker 
+                                    sx={{margin: 2}} 
+                                    label="Enter End Date" 
+                                    format="MMM YY"
+                                />
+                            </LocalizationProvider>
+                        </div>
+                    );
+                })
+                
+            }
         </>
             
        
--- src/components/cvcomponents/ExperienceSection.js
+++ src/components/cvcomponents/ExperienceSection.js
@@ -13,7 +13,7 @@ function ExperienceSection() {
                                                                     roleDetails: "",
                                                                     startDate: "",
                                                                     endDate:""
-                                                                }])
+                                                                }]);
     
 
     const handleInputChange = (index, event) =>{

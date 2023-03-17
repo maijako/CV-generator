82ed85860f088f92b4ab66460db1bb266d4de0e7
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-17 06:18:19 +0000

    Adds comments for the different parts of the project section to be implemented

---
+++ src/components/cvcomponents/ProjectsSection.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ProjectsSection() {
+    return(
+        <>
+            <h3>Project Details</h3>
+            {
+
+            }
+            {/* Project Name,
+                Summary,
+                Skills Used,
+                Deployed Link,
+                GitHub Link */}
+        </>
+    );
+}
+
+export default ProjectsSection;

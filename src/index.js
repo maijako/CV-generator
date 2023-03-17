865f892e2f10005638fd2b541d58ab1aeb812aa0
ensignjava <109529105+maijako@users.noreply.github.com>
2023-03-17 17:37:36 +0000

    install react redux toolkit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+//import the created Redux store and wrap app in provider elements
+import store from "./state/store";
+import { Provider } from "react-redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 

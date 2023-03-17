6d643aaaff84f760fe41c481bd6b0dc1da86c085
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-17 06:36:29 +0000

    Adds link to main form page, and some text fields to the project section

--- src/components/cvcomponents/ProjectsSection.js
+++ src/components/cvcomponents/ProjectsSection.js
@@ -1,4 +1,7 @@
 import React from "react";
+import DescriptionIcon from '@mui/icons-material/Description';
+import TitleIcon from '@mui/icons-material/Title';
+import { TextField, InputAdornment } from '@mui/material';
 
 function ProjectsSection() {
     return(
@@ -12,6 +15,47 @@ function ProjectsSection() {
                 Skills Used,
                 Deployed Link,
                 GitHub Link */}
+
+            <div className="containerStyles">
+            
+                <TextField
+                    id="input-project-name"
+                    label="Project Title"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <TitleIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                    placeholder="Enter Project Title"
+                    variant="outlined"
+                    sx={{margin: "15px 0"}}
+                    // name="instituteName"
+                    // value={input.instituteName}
+                    // onChange={event => handleInputChange(index, event)}
+                />
+
+                <TextField
+                    id="project-summary"
+                    label="Summary"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <DescriptionIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                    placeholder="Enter a short summary of the project..."
+                    multiline
+                    minRows={3}
+                    maxRows={8}
+                    sx={{width: "50%"}}
+                    // name="roleDetails"
+                    // value={input.roleDetails}
+                    // onChange={event => handleInputChange(index, event)}
+                />
+            </div>
         </>
     );
 }

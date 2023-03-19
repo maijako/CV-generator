f3b69705abfcc49306a1599d40d820a407168f15
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-03-19 18:00:26 +0000

    Adds a modal for getting user input for generating professional summary

--- src/components/ChatGPTAI.js
+++ src/components/ChatGPTAI.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import {Configuration, OpenAIApi} from "openai";
 import APIKEY from "../APIKey.json";
+
+import Box from '@mui/material/Box';
+import { TextField, Button } from "@mui/material";
+import Modal from '@mui/material/Modal';
 
 //import useDispatch and useSElector hooks from react-redux into the form
 import { useDispatch} from "react-redux";
 import { setSummary } from "../state/summary";
 
 function ChatGPTAI() {
+
+    const [open, setOpen] = useState(false);
+    const [numPrevExperience, setNumPrevExperience] = useState(0);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+
+    const handlePrevExperience = (event) => {
+        const value = Number(event.target.value);
+        console.log(value);
+        if(isNaN(value)){
+            console.log("Please Enter a valid number");
+            return;
+        }else if(value<0 || value > 25) {
+            console.log("exp not in range");
+        }else{
+            setNumPrevExperience(value);
+        }
+        
+    }
     const dispatch = useDispatch();
     const configuration = new Configuration({
       apiKey: APIKEY.APIKeyAI,
@@ -40,6 +75,35 @@ function ChatGPTAI() {
   return (
     <div>
       <button onClick={getResponse}>Genterate Response</button>
+      <Button onClick={handleOpen}>Summary Suggestion</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+            <TextField
+                id="input-previous-role"
+                label="Previous Role"
+                name="prevRole"
+                placeholder="Enter Previous Role"
+                variant="outlined"
+                sx={{ margin: "15px 0" }}
+            />
+
+            <TextField
+                id="input-previous-experience"
+                label="Previous Experience"
+                name="prevExperience"
+                placeholder="Enter Previous Experience"
+                variant="outlined"
+                value={numPrevExperience}
+                onChange={(event) => handlePrevExperience(event)}
+                sx={{ margin: "15px 0" }}
+            />
+        </Box>
+      </Modal>
     </div>
   );
 }

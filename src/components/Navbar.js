c39ff3b96837cca2f6eaa19990130e9dc2b1828a
ensignjava <109529105+maijako@users.noreply.github.com>
2023-03-20 15:32:16 +0000

    home page styling mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,38 +2,41 @@ import React from "react";
 import "../styles/Navbar.css";
 import { NavLink } from 'react-router-dom';
 
-// const Navbar = () => {
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark ">
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark ">
                 <span className="navbar-brand nav__logo">CV Builder</span>
-                <button className="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <div className="d-flex justify-content-end">
+                <button className="navbar-toggler ml-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" ></span>
                 </button>
-
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav mt-2 mt-lg-0 ms-auto">
-                        <li className="nav-item">
-                            <NavLink to="/" className="nav-link">
-                                HOME
-                            </NavLink>
-                        </li>
-
-                        <li className="nav-item">
-                            <NavLink to="buildCV" className="nav-link">
-                                BUILD CV
-                            </NavLink>
-                        </li>
-
-                        <li className="nav-item">
-                            <NavLink to="preview" className="nav-link">
-                                PREVIEW
-                            </NavLink>
-                        </li>
-                    </ul>
                 </div>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav mt-2 mt-lg-0 ms-auto">
+                    <li className="nav-item">
+                        <NavLink to="/" className="nav-link">
+                            HOME
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="buildCV" className="nav-link">
+                            BUILD CV
+                        </NavLink>
+                    </li>
+
+                    <li className="nav-item">
+                        <NavLink to="preview" className="nav-link">
+                            PREVIEW
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav >
+     </>
     );
 };
 
